2e13ca86451ec953f3cdff1e530807b656d53401
gmvald <gmvald@uiowa.edu>
2025-02-04 18:02:12 -0600

    Add .gitignore, restructure standardplot.py, update executionPlot.py, and enhance README

--- assignmentOne_PythonRefresher/executionPlot.py
+++ assignmentOne_PythonRefresher/executionPlot.py
@@ -2,9 +2,12 @@ import matplotlib.pyplot as plt
 import numpy as np
 import pandas as pd
 
-data = pd.read_csv("fibTimeLog.csv")
+data = pd.read_csv("assignmentOne_PythonRefresher/fibTimeLog.csv")
 df = pd.DataFrame(data)
 print(df)
 
 plt.plot(df["input"], df["executionTime"])
+plt.title("Execution Time of fib(100): With Cache")
+plt.xlabel("Fib(n)")
+plt.ylabel("Execution Time (s)")
 plt.show()
--- assignmentOne_PythonRefresher/fib.py
+++ assignmentOne_PythonRefresher/fib.py
@@ -18,7 +18,7 @@ def timer(func):
 
 def time_logger(func):
     # This is an additional decorator that I created so that I can log the execution time of a function directly to a csv, rather than just crudely adding the functionality to the timer decorator.
-    csvName = f"{func.__name__}TimeLog.csv"
+    csvName = f"assignmentOne_PythonRefresher/{func.__name__}TimeLog.csv"
 
     with open(csvName, "w", newline="") as file:
         writer = csv.writer(file)
@@ -47,6 +47,29 @@ def fib(n: int) -> int:
     else:
         return fib(n - 2) + fib(n - 1)
 
-
 if __name__ == "__main__":
     fib(100)
+
+
+"""
+Output when function is run:
+
+Finished in 0.00000000s: f(0) -> 0
+Finished in 0.00000000s: f(1) -> 1   
+Finished in 0.00100088s: f(2) -> 1   
+Finished in 0.00000000s: f(3) -> 2   
+Finished in 0.00200176s: f(4) -> 3   
+Finished in 0.00099874s: f(5) -> 5   
+Finished in 0.00300050s: f(6) -> 8   
+Finished in 0.00000000s: f(7) -> 13  
+Finished in 0.00399995s: f(8) -> 21  
+Finished in 0.00000000s: f(9) -> 34  
+Finished in 0.00399995s: f(10) -> 55 
+...
+Finished in 0.00000000s: f(95) -> 31940434634990099905
+Finished in 0.04300117s: f(96) -> 51680708854858323072
+Finished in 0.00000000s: f(97) -> 83621143489848422977
+Finished in 0.04300117s: f(98) -> 135301852344706746049
+Finished in 0.00000000s: f(99) -> 218922995834555169026
+Finished in 0.04400110s: f(100) -> 354224848179261915075
+"""
