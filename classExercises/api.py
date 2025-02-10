7ae5ff0e5bec294fc4955ef991e167ab91a8e0a7
gabeVald <gmvald@uiowa.edu>
2025-02-10 11:40:00 -0600

    weekend goofin
    don't push .env files

---
+++ classExercises/api.py
@@ -0,0 +1,38 @@
+from fastapi import FastAPI
+from openai import OpenAI
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+api_key = os.environ["OPENAI_API_KEY"]
+client = OpenAI(api_key=api_key)
+
+app = FastAPI()
+
+
+@app.get("/")
+async def welcome() -> dict:
+    return {"msg": "Hello World!"}
+
+
+@app.get("/chatbot")
+async def chat(message) -> dict:
+    if message:
+        completion = client.chat.completions.create(
+            model="gpt-4o-mini",
+            messages=[
+                {
+                    "role": "system",
+                    "content": "Sarcastically respond to the user, as if their question is the dumbest thing you've ever heard.",
+                },
+                {"role": "user", "content": f"{message}"},
+            ],
+        )
+        print(completion)
+        return {
+            "msg": f"{completion.choices[0].message}",
+            "tokens": {completion.usage.completion_tokens},
+        }
+    else:
+        return {"msg": "There isn't a user message!"}
