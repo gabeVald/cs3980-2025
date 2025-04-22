a947ac09b15c506bfb5331c42a06030332fb0e63
gabeVald <gmvald@uiowa.edu>
2025-04-22 12:33:41 -0500

    Refactor Todo router to improve code readability; remove deprecated files and enhance ID handling

--- classExercises/todo/backend/routers/todo.py
+++ classExercises/todo/backend/routers/todo.py
@@ -1,12 +1,14 @@
-#GET FROM HIS DEMO
+# GET FROM HIS DEMO
 from fastapi import APIRouter, Path, HTTPException, status
 
 from models.todo import Todo, TodoRequest
 
 todo_router = APIRouter()
 
-todo_list = []
-max_id: int = 0 # need to get rid of this, so use either get max ID from database to get this when the db starts, or mongodbObjectID. Look at movies example in his demo.
+
+max_id: int = (
+    0  # need to get rid of this, so use either get max ID from database to get this when the db starts, or mongodbObjectID. Look at movies example in his demo.
+)
 
 
 @todo_router.post("", status_code=status.HTTP_201_CREATED)
@@ -15,7 +17,9 @@ async def add_todo(todo: TodoRequest) -> Todo:
     max_id += 1  # auto increment ID
 
     newTodo = Todo(id=max_id, title=todo.title, description=todo.description)
-    await Todo.insert_one(newTodo) #since classname is inherited from document class, we can connect to db directly from Todo.mongo_commands
+    await Todo.insert_one(
+        newTodo
+    )  # since classname is inherited from document class, we can connect to db directly from Todo.mongo_commands
     return newTodo
 
 
@@ -24,7 +28,9 @@ async def get_todos() -> list[Todo]:
     return await Todo.find_all().to_list()
 
 
-@todo_router.get("/{id}") # Look at movies example to see authentication for user specific id
+@todo_router.get(
+    "/{id}"
+)  # Look at movies example to see authentication for user specific id
 async def get_todo_by_id(id: int = Path(..., title="default")) -> Todo:
     todo = await Todo.get(id)
     if todo:
@@ -42,14 +48,14 @@ async def update_todo(todo: TodoRequest, id: int) -> dict:
         existing_todo.title = todo.title
         existing_todo.description = todo.description
         await existing_todo.save()
-        return {"message":"Todo updated successfully"}
+        return {"message": "Todo updated successfully"}
 
     return {"message": f"The todo with ID={id} is not found."}
 
 
 @todo_router.delete("/{id}")
 async def delete_todo(id: int) -> dict:
-    
+
     todo = await Todo.get(id)
     if todo:
         await todo.delete()
